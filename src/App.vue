36bf1037c7f34b770492a325c8f88c6b9b29bc9e
DESKTOP-6PS5KK6\幾花 <mark88915@gmail.com>
2023-03-15 15:42:10 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import SwiperHeader from './components/SwiperHeader.vue';
+import Swiper from './components/Swiper.vue';
+</script>
+
+<template>
+    <SwiperHeader />
+    <Swiper />
+</template>
---
+++ src/components/SwiperHeader.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="header">
+        <h1 id="title">Swiper Practice</h1>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.header {
+    height: 100px;
+    width: 100%;
+    background-color: bisque;
+
+    #title {
+        text-align: center;
+
+        position: relative;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+}
+</style>
